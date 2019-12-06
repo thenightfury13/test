615ae52826f99580e2490f373a6b562bcefff91b
thenightfury13 <thenightfury13@gmail.com>
2019-12-06 12:42:22 +0000

    changes are made

--- calc.c
+++ calc.c
@@ -8,16 +8,16 @@ int main() {
     scanf("%lf %lf", &first, &second);
     switch (operator) {
     case '+':
-        printf("%.1lf + %.1lf = %.1lf", first, second, first + second);
+        printf("%.1lf + %.1lf = %.1lf \n", first, second, first + second);
         break;
     case '-':
-        printf("%.1lf - %.1lf = %.1lf", first, second, first - second);
+        printf("%.1lf - %.1lf = %.1lf \n", first, second, first - second);
         break;
     case '*':
-        printf("%.1lf * %.1lf = %.1lf", first, second, first * second);
+        printf("%.1lf * %.1lf = %.1lf \n", first, second, first * second);
         break;
     case '/':
-        printf("%.1lf / %.1lf = %.1lf", first, second, first / second);
+        printf("%.1lf / %.1lf = %.1lf \n", first, second, first / second);
         break;
     default:
         printf("Operator is not correct");
